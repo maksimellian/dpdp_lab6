607d51b8bad9832a29bca43cca2541cc9cfb71bd
maksimellian <maksimellian01@yandex.ru>
2022-01-07 23:09:10 +0300

    Scripted auto-commit on change (2022-01-07 23:09:10) by gitwatch.sh

--- src/main/java/ZookeeperApp.java
+++ src/main/java/ZookeeperApp.java
@@ -1,5 +1,8 @@
+import java.time.Duration;
+
 public class ZookeeperApp {
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
     private static final String URL = "url";
+    private final static Duration TIMEOUT = Duration.ofSeconds(5);
 }
