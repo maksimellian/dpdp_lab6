c1212c98c7ae1d8045ed9e5500bcf2034c503ad9
maksimellian <maksimellian01@yandex.ru>
2022-01-07 23:49:36 +0300

    Scripted auto-commit on change (2022-01-07 23:49:36) by gitwatch.sh

--- src/main/java/ConfigStorageActor.java
+++ src/main/java/ConfigStorageActor.java
@@ -3,9 +3,9 @@ import akka.actor.AbstractActor;
 import java.util.ArrayList;
 
 public class ConfigStorageActor extends AbstractActor {
-    private ArrayList<>
+    private ArrayList<String> servers;
     @Override
     public Receive createReceive() {
-        return null;
+        return 
     }
 }
