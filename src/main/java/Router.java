95bc37a34d106eec38d1278d2115e6dc47e4d25d
maksimellian <maksimellian01@yandex.ru>
2022-01-09 14:08:04 +0300

    Scripted auto-commit on change (2022-01-09 14:08:04) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -28,7 +28,7 @@ public class Router {
                                 return completeWithFuture(
                                         Patterns.ask(this.configActor, new EmptyServersMessage(), TIMEOUT)
                                                 .thenApply(serverUrl -> (String)serverUrl)
-                                                .thenCompose((serverUrl) -> ))
+                                                .thenCompose((serverUrl) -> this.http.singleRequest()))
                             }
                         }))
                 )
