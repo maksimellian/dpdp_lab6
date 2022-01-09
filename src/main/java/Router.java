0ecab3b28f978e15d3e5696fc8afa67c9f75fe2e
maksimellian <maksimellian01@yandex.ru>
2022-01-09 13:34:20 +0300

    Scripted auto-commit on change (2022-01-09 13:34:20) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,10 +1,14 @@
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 
+import java.time.Duration;
+
 public class Router {
     private final ActorRef configActor;
     private final Http http;
-    private final String url = "url";
+    private final String URL = "url";
+    private final String COUNT = "count";
+    private final Duration TIMEOUT = Duration.ofSeconds(5);
     public Router(ActorRef configActor, Http http) {
         this.configActor = configActor;
         this.http = http;
