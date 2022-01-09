1f918a9c126ebaddaa3f39f7e9e395d116171e16
maksimellian <maksimellian01@yandex.ru>
2022-01-09 03:54:20 +0300

    Scripted auto-commit on change (2022-01-09 03:54:20) by gitwatch.sh

--- src/main/java/ConfigStorageActor.java
+++ src/main/java/ConfigStorageActor.java
@@ -9,7 +9,7 @@ public class ConfigStorageActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(ServersMessage.class, msg -> this.servers = msg.getUrls())
-                .match(EmptyServersMessage.class, msg ->)
+                .match(EmptyServersMessage.class, msg -> sender().tell())
                 .build();
     }
 }
