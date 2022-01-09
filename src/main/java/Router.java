f1fb8bebef0d43c018866c6e9ca6b3c1e0c9e7cd
maksimellian <maksimellian01@yandex.ru>
2022-01-09 14:16:38 +0300

    Scripted auto-commit on change (2022-01-09 14:16:38) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -24,7 +24,8 @@ public class Router {
 
     public String createUrl(String serverUrl, String url, int count) {
         String link = String.valueOf(Uri.create(serverUrl).query(Query.create(
-                Pair.create(URL, url)
+                Pair.create(URL, url),
+                Pair.create()
         )))
     }
 
