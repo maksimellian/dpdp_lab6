13f8d13b1547af1068fadbf335155fa32f0167fa
maksimellian <maksimellian01@yandex.ru>
2022-01-09 14:15:47 +0300

    Scripted auto-commit on change (2022-01-09 14:15:47) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,6 +1,7 @@
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.Uri;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 
@@ -20,7 +21,7 @@ public class Router {
     }
 
     public String createUrl(String serverUrl, String url, int count) {
-        String link = String.valueOf()
+        String link = String.valueOf(Uri.create())
     }
 
     public Route createRoute() {
