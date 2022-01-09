c83ac56c2a7b2beb30da003cc5c6ac2aac81aa47
maksimellian <maksimellian01@yandex.ru>
2022-01-09 13:25:34 +0300

    Scripted auto-commit on change (2022-01-09 13:25:34) by gitwatch.sh

--- src/main/java/ZooWatcher.java
+++ src/main/java/ZooWatcher.java
@@ -2,8 +2,10 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZooWatcher implements Watcher {
+    private final String HOST = "127.0.0.1";
+    private final String PORT = "2181";
+    private final TIMEOUT =
     @Override
     public void process(WatchedEvent event) {
-
     }
 }
