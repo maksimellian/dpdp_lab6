cfc109a1e60f5bef38bf265ab5730c429301ea70
maksimellian <maksimellian01@yandex.ru>
2022-01-09 13:32:24 +0300

    Scripted auto-commit on change (2022-01-09 13:32:24) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,2 +1,12 @@
+import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
+
 public class Router {
+    private final ActorRef configActor;
+    private final Http http;
+    private final String url = "url";
+    public Router(ActorRef configActor, Http http) {
+        this.configActor = configActor;
+        this.http = http;
+    }
 }
