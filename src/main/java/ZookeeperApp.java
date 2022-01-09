750351d95bca0127421f8ae189afe531fd7ae5cb
maksimellian <maksimellian01@yandex.ru>
2022-01-09 04:07:53 +0300

    Scripted auto-commit on change (2022-01-09 04:07:53) by gitwatch.sh

--- src/main/java/ZookeeperApp.java
+++ src/main/java/ZookeeperApp.java
@@ -1,3 +1,8 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.dsl.Creators;
+
 import java.time.Duration;
 
 public class ZookeeperApp {
@@ -6,6 +11,10 @@ public class ZookeeperApp {
     private static final String URL = "url";
     private final static Duration TIMEOUT = Duration.ofSeconds(5);
     private static void main(String[] args) {
-        
+        final ActorSystem system = ActorSystem.create("routes");
+        ActorRef configStorageActor = system.actorOf(Props.create(ConfigStorageActor.class));
+        if (args.length != 0) {
+            PORT = Integer.parseInt(args[0]);
+        }
     }
 }
