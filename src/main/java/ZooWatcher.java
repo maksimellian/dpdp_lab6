7deb2366ed61f788b2b2eec3948f22fb39678884
maksimellian <maksimellian01@yandex.ru>
2022-01-09 13:22:56 +0300

    Scripted auto-commit on change (2022-01-09 13:22:56) by gitwatch.sh

--- src/main/java/ZooWatcher.java
+++ src/main/java/ZooWatcher.java
@@ -1,4 +1,9 @@
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZooWatcher implements Watcher {
+    @Override
+    public void process(WatchedEvent event) {
+
+    }
 }
