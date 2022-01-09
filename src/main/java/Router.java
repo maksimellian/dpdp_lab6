b116cfe8c04962b02bfab5e7039fa41f877e6887
maksimellian <maksimellian01@yandex.ru>
2022-01-09 14:27:17 +0300

    Scripted auto-commit on change (2022-01-09 14:27:17) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,14 +1,15 @@
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.Query;
 import akka.http.javadsl.model.Uri;
 import akka.http.javadsl.server.Route;
-import akka.http.javadsl.server.directives.RouteAdapter;
 import akka.japi.Pair;
 import akka.pattern.Patterns;
 
 import java.time.Duration;
+import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
 
@@ -31,13 +32,12 @@ public class Router {
         return link;
     }
 
-    public RouteAdapter sendToRandomServer(String url, String count) {
-        return completeWithFuture(
-                Patterns.ask(this.configActor, new EmptyServersMessage(), TIMEOUT)
-                        .thenApply(serverUrl -> (String)serverUrl)
-                        .thenCompose((serverUrl) ->
-                                this.http.singleRequest(HttpRequest
-                                        .create(this.createUrl(serverUrl, url, Integer.parseInt(count))))));
+    public CompletionStage<HttpResponse> sendToRandomServer(String url, String count) {
+         return Patterns.ask(this.configActor, new EmptyServersMessage(), TIMEOUT)
+                .thenApply(serverUrl -> (String)serverUrl)
+                .thenCompose((serverUrl) ->
+                        this.http.singleRequest(HttpRequest
+                                .create(this.createUrl(serverUrl, url, Integer.parseInt(count)))));
     }
 
     public Route createRoute() {
@@ -47,8 +47,7 @@ public class Router {
                             if (Integer.parseInt(count) == 0){
                                 return completeWithFuture(this.http.singleRequest(HttpRequest.create(url)));
                             } else {
-                                // send to random server
-                                sendToRandomServer(url, count);
+                                return completeWithFuture(sendToRandomServer(url, count));
                             }
                         }))
                 )
