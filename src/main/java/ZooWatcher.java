6e999ef2ead41f8c9e4aa13014db39529a2ab65c
maksimellian <maksimellian01@yandex.ru>
2022-01-09 14:42:13 +0300

    Scripted auto-commit on change (2022-01-09 14:42:13) by gitwatch.sh

--- src/main/java/ZooWatcher.java
+++ src/main/java/ZooWatcher.java
@@ -1,14 +1,24 @@
 import akka.actor.ActorRef;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 import java.time.Duration;
 
 public class ZooWatcher implements Watcher {
     private final String HOST = "127.0.0.1";
     private final String PORT = "2181";
-    private final ActorRef
+    private final String ADDRESS = "127.0.0.1:2181"
     private final Duration TIMEOUT = Duration.ofSeconds(5);
+    private final ActorRef configActor;
+    private final ZooKeeper zoo;
+
+
+    public ZooWatcher(ActorRef configActor) {
+        this.configActor = configActor;
+        this.zoo = new ZooKeeper()
+    }
+
     @Override
     public void process(WatchedEvent event) {
     }
