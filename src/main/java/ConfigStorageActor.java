55671e0207b5158b0d35a386858d344b42fe61e9
maksimellian <maksimellian01@yandex.ru>
2022-01-09 03:57:49 +0300

    Scripted auto-commit on change (2022-01-09 03:57:49) by gitwatch.sh

--- src/main/java/ConfigStorageActor.java
+++ src/main/java/ConfigStorageActor.java
@@ -2,6 +2,7 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ConfigStorageActor extends AbstractActor {
     private ArrayList<String> servers;
@@ -9,7 +10,7 @@ public class ConfigStorageActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(ServersMessage.class, msg -> this.servers = msg.getUrls())
-                .match(EmptyServersMessage.class, msg -> sender().tell())
+                .match(EmptyServersMessage.class, msg -> sender().tell(servers.get(new Random().nextInt(servers.size())), self()))
                 .build();
     }
 }
